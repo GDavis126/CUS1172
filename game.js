c4cfc0f1475ac61023a145c5718aeb88a2798130
GDavis126 <55218867+GDavis126@users.noreply.github.com>
2020-10-30 22:20:09 -0400

    Add files via upload

---
+++ game.js
@@ -0,0 +1,79 @@
+const question = document.getElementById('question');
+const choices = Array.from(document.getElementsByClassName('choice-text'));
+
+let currentQuestion = {};
+let acceptingAnswers = false;
+let score = 0;
+let questionCounter = 0;
+let availableQuesions = [];
+
+let questions = [
+    {
+        question: 'What is 2+2??',
+        choice1: '4',
+        choice2: '20',
+        choice3: '8',
+        choice4: '5',
+        answer: 1,
+    },
+    {
+        question:
+            "What is 10*10?",
+        choice1: "1000",
+        choice2: "20",
+        choice3: "100",
+        choice4: "25",
+        answer: 3,
+    },
+    {
+        question: " What is the square root of 64",
+        choice1: "124",
+        choice2: "32",
+        choice3: "4",
+        choice4: "8",
+        answer: 4,
+    },
+];
+
+//CONSTANTS
+const CORRECT_BONUS = 10;
+const MAX_QUESTIONS = 3;
+
+startGame = () => {
+    questionCounter = 0;
+    score = 0;
+    availableQuesions = [...questions];
+    getNewQuestion();
+};
+
+getNewQuestion = () => {
+    if (availableQuesions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign('/end.html');
+    }
+    questionCounter++;
+    const questionIndex = Math.floor(Math.random() * availableQuesions.length);
+    currentQuestion = availableQuesions[questionIndex];
+    question.innerText = currentQuestion.question;
+
+    choices.forEach((choice) => {
+        const number = choice.dataset['number'];
+        choice.innerText = currentQuestion['choice' + number];
+    });
+
+    availableQuesions.splice(questionIndex, 1);
+    acceptingAnswers = true;
+};
+
+choices.forEach((choice) => {
+    choice.addEventListener('click', (e) => {
+        if (!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset['number'];
+        getNewQuestion();
+    });
+});
+
+startGame();
